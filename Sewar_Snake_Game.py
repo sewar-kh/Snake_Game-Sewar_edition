1cfc7397df02f2d91187c6b180fc4a05443d4265
sewar-kh <151630862+sewar-kh@users.noreply.github.com>
2025-07-04 19:55:04 +0300

    Milestone #2: Animate
    Each time through the animation loop the snake should move player 20 pixels in the direction it is traveling. The directions are either left, right, up or down. At the start the snake should be traveling to the right
    
    Once this milestone is completed, the program only animates the snake moving right.

--- Sewar_Snake_Game.py
+++ Sewar_Snake_Game.py
@@ -6,9 +6,17 @@ CANVAS_WIDTH = 400
 CANVAS_HEIGHT = 400
 SIZE = 20
 
-def main():
-    intiate_game()
+# if you make this larger, the game will go slower
+DELAY = 0.1 
 
+def main():
+    #intiate Canvas 
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    canvas.clear()
+    
+    snake_x, snake_y ,snake_id = intiate_game(canvas)
+    animate(canvas, snake_x, snake_y,snake_id)
+    
 def draw_square(canvas,x,y,color):
     size = SIZE
     left_x = x
@@ -30,22 +38,37 @@ def draw_snake(canvas,x,y,color):
 def draw_food(canvas,x,y,color):
     return draw_square(canvas,x,y,color)
 
-def intiate_game():
-
- #intiate Canvas 
-    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    canvas.clear()
-
-   # add the food starting randomaly 
-    food_x = random.randint(0, (CANVAS_WIDTH - SIZE) // SIZE) * SIZE
-    food_y = random.randint(0, (CANVAS_HEIGHT - SIZE) // SIZE) * SIZE
-    food_id = draw_food(canvas,food_x,food_y,"red")
+def intiate_game(canvas):
     
  # Create the snake starting at top-left corner
     snake_x = 0
     snake_y = 0
     snake = [(snake_x, snake_y)]
-    snake_ids = [draw_snake(canvas, snake_x, snake_y, "blue")]
+    snake_id = draw_snake(canvas, snake_x, snake_y, "blue")
+
+# add the food starting randomaly 
+    food_x = random.randint(0, (CANVAS_WIDTH - SIZE) // SIZE) * SIZE
+    food_y = random.randint(0, (CANVAS_HEIGHT - SIZE) // SIZE) * SIZE
+    food_id = draw_food(canvas,food_x,food_y,"red")
+    
+    return snake_x, snake_y , snake_id
+
+def animate(canvas, snake_x, snake_y,snake_id):
+    direction = 'right'  # Start moving to the right
+
+    while True:  # animation loop
+        time.sleep(DELAY)
+
+        #  Move the snake in the current direction - move 20 pixels in the current direction
+
+        snake_x += SIZE
+
+        # Move snake to new position
+        canvas.moveto(snake_id, snake_x, snake_y)
+       
+
+   
+   
 
 if __name__ == '__main__':
     main()
